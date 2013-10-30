5b646d40cdaab7cf275dba44f89f9df930184883
= <=>
2013-10-30 16:45:38 +0100

    update model

--- src/Boyhagemann/Pages/Model/Page.php
+++ src/Boyhagemann/Pages/Model/Page.php
@@ -94,7 +94,7 @@ class Page extends \Eloquent
         $page = self::createWithContent($title, $route, $controller . '@' . $action, $layout, $method, $alias);
 
 		Event::fire('page.createResourcePage', array($page, $action));
-		
+
         return $page;
     }
 
